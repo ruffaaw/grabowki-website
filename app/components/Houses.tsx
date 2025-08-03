e60a57c64054ea529483857d9d0aaa8db619e748
ruffaaw <rafalciupek07@gmail.com>
2025-08-03 23:28:25 +0200

    fix: removed unnecessery import

--- app/components/Houses.tsx
+++ app/components/Houses.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { useState } from "react";
 import { FaBed, FaDollarSign, FaFile, FaLeaf } from "react-icons/fa";

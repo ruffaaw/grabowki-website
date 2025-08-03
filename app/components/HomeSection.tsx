dffe307a3d861f8f1e25e80ea3b94cde8756408c
ruffaaw <rafalciupek07@gmail.com>
2025-08-03 23:50:13 +0200

    feat: responsiveness

--- app/components/HomeSection.tsx
+++ app/components/HomeSection.tsx
@@ -6,16 +6,16 @@ export default function HomeSection() {
         style={{ backgroundImage: "url('/background_image.png')" }}
       />
 
-      <div className="relative z-10 flex flex-col items-start justify-end h-full text-start px-12 py-10">
-        <h1 className="text-white text-9xl font-normal drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)]">
+      <div className="relative z-10 flex flex-col items-center md:items-start justify-end h-full text-start px-12 py-10">
+        <h1 className="max-md:hidden text-white text-8xl lg:text-9xl font-normal drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)]">
           OSIEDLE
         </h1>
-        <h1 className="text-white text-[300px] font-normal drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)] -mt-24 -mb-24">
+        <h1 className="max-md:hidden text-white text-[160px] lg:text-[250px] xl:text-[300px] font-normal drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)] -my-12 lg:-mt-16 lg:-mb-18 xl:-mt-24 xl:-mb-24">
           GRABÓWKI
         </h1>
         <a
           href="/oferta"
-          className="bg-[var(--themeBlueDark)] text-white text-[40px] px-10 py-3 hover:bg-gray-200 transition drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)]"
+          className="bg-[var(--themeBlueDark)] text-white text-3xl md:text-4xl lg:text-[40px] px-10 py-3 hover:bg-gray-200 transition drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)] text-center "
         >
           Zobacz ofertę
         </a>

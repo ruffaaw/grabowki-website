506f5d3e4a8087a701ec638cc8642bcd8a1d5f16
ruffaaw <rafalciupek07@gmail.com>
2025-08-04 00:12:52 +0200

    responsiveness

--- app/components/AboutInvestment.tsx
+++ app/components/AboutInvestment.tsx
@@ -5,16 +5,18 @@ export default function AboutInvestment() {
   return (
     <section
       id="o-inwestycji"
-      className="flex flex-col px-12 py-16 bg-transparent bg-gradient-to-b from-[#D5EBF9] to-[#E8E8E8]"
+      className="flex flex-col  py-16 bg-transparent bg-gradient-to-b from-[#D5EBF9] to-[#E8E8E8]"
     >
-      <h1 className="text-[40px] mb-5 font-medium text-black">O INWESTYCJI</h1>
+      <h1 className="text-[40px] mb-5 font-medium text-black px-6 md:px-12">
+        O INWESTYCJI
+      </h1>
       <div className="flex flex-col space-y-16">
-        <div className="flex flex-row items-start justify-between space-x-8">
-          <div className="flex-1 flex flex-col space-y-5">
-            <h2 className="text-[48px] font-semibold bg-gradient-to-r from-[#24425c] to-[#4d7a9d] bg-clip-text text-transparent w-fit">
+        <div className="flex flex-col md:flex-row items-start justify-between space-y-8 md:space-x-8 md:px-12">
+          <div className="flex-1 flex flex-col space-y-5 max-md:px-6">
+            <h2 className="text-[40px] md:text-[40px] font-semibold bg-gradient-to-r from-[#24425c] to-[#4d7a9d] bg-clip-text text-transparent w-fit">
               GRABÓWKI TWÓJ NOWY LEPSZY ADRES
             </h2>
-            <p className="text-3xl font-semibold text-[#1A2F42] text-left">
+            <p className="text-2xl md:text-3xl font-semibold text-[#1A2F42] text-left">
               Zamknięta przestrzeń zapewnia mieszkańcom komfort
               i&nbsp;prywatność, a&nbsp;oświetlone drogi wewnętrzne
               i&nbsp;miejsca postojowe gwarantują bezpieczne powroty do domu
@@ -26,7 +28,7 @@ export default function AboutInvestment() {
             </p>
           </div>
 
-          <div className="relative aspect-[4/3] flex-1 max-w-[550px] max-h-[450px] ">
+          <div className="relative aspect-[4/3] flex-1 w-full md:max-w-[550px] max-h-[450px]">
             <Image
               src="/image4.png"
               alt="Zdjęcie inwestycji"
@@ -37,8 +39,8 @@ export default function AboutInvestment() {
           </div>
         </div>
 
-        <div className="flex flex-row items-start justify-between space-x-8">
-          <div className="relative aspect-[4/3] flex-1 max-w-[550px] max-h-[450px] ">
+        <div className="flex flex-col-reverse md:flex-row items-start justify-between md:space-x-8 md:px-12">
+          <div className="relative aspect-[4/3] flex-1 w-full md:max-w-[550px] max-h-[450px] max-md:mt-8">
             <Image
               src="/image3.png"
               alt="Zdjęcie inwestycji"
@@ -47,12 +49,12 @@ export default function AboutInvestment() {
               priority={false}
             />
           </div>
-          <div className="flex-1 flex flex-col space-y-5">
-            <h2 className="text-[48px] font-semibold bg-gradient-to-r from-[#24425c] to-[#4d7a9d] bg-clip-text text-transparent w-fit">
+          <div className="flex-1 flex flex-col space-y-5 max-md:px-6">
+            <h2 className="text-[40px] md:text-[48px] font-semibold bg-gradient-to-r from-[#24425c] to-[#4d7a9d] bg-clip-text text-transparent w-fit">
               NOWE TECHNOLOGIE <br />
               I&nbsp;EKOLOGICZNE ROZWIĄZANIA
             </h2>
-            <p className="text-3xl font-semibold text-[#1A2F42] text-left">
+            <p className="text-2xl md:text-3xl font-semibold text-[#1A2F42] text-left">
               Inwestycja została zaprojektowana z&nbsp;myślą o&nbsp;komforcie
               i&nbsp;zdrowiu mieszkańców. Każde mieszkanie oferuje dobrze
               doświetlone wnętrza oraz przestrzeń do relaksu – zarówno wewnątrz,
@@ -62,12 +64,12 @@ export default function AboutInvestment() {
           </div>
         </div>
 
-        <div className="flex flex-row items-start justify-between space-x-8">
-          <div className="flex-1 flex flex-col space-y-5">
-            <h2 className="text-[48px] font-semibold bg-gradient-to-r from-[#24425c] to-[#4d7a9d] bg-clip-text text-transparent w-fit">
+        <div className="flex flex-col md:flex-row items-start justify-between max-md:space-y-8 md:space-x-8 md:px-12">
+          <div className="flex-1 flex flex-col space-y-5 max-md:px-6">
+            <h2 className="text-[40px] md:text-[48px] font-semibold bg-gradient-to-r from-[#24425c] to-[#4d7a9d] bg-clip-text text-transparent w-fit">
               IDEALNE MIEJSCE DLA CIEBIE{" "}
             </h2>
-            <p className="text-3xl font-semibold text-[#1A2F42] text-left">
+            <p className="text-2xl md:text-3xl font-semibold text-[#1A2F42] text-left">
               Znajdziesz tu malownicze tereny zielone, idealne na codzienne
               spacery, jogging czy rodzinne pikniki na świeżym powietrzu.
               Okolica sprzyja rekreacji i&nbsp;aktywnemu trybowi życia, oferując
@@ -78,7 +80,7 @@ export default function AboutInvestment() {
             </p>
           </div>
 
-          <div className="relative aspect-[4/3] flex-1 max-w-[550px] max-h-[450px] ">
+          <div className="relative aspect-[4/3] flex-1 w-full md:max-w-[550px] max-h-[450px] ">
             <Image
               src="/image2.png"
               alt="Zdjęcie inwestycji"
@@ -88,17 +90,17 @@ export default function AboutInvestment() {
             />
           </div>
         </div>
-        <div className="flex flex-col items-start space-y-7">
-          <h2 className="text-[48px] font-semibold text-[var(--themeBlueDark)] w-fit">
+        <div className="flex flex-col items-start space-y-7 px-6 md:px-12">
+          <h2 className="text-[40px] md:text-[48px] font-semibold text-[var(--themeBlueDark)] w-fit">
             Twój nowy dom na osiedlu Grabówki – połączenie spokoju
             i&nbsp;nowoczesnego komfortu na wyciągnięcie ręki.
           </h2>
           <a
             href="#kontakt"
-            className="bg-[var(--themeBlueDark)] text-white text-[32px] px-24 py-3 font-semibold hover:bg-gray-200 transition drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)] flex items-center gap-4 w-fit"
+            className="bg-[var(--themeBlueDark)] text-white text-2xl md:text-3xl lg:text-[32px] px-8 md:px-24 py-3 font-semibold hover:bg-gray-200 transition drop-shadow-[10px_15px_25px_rgba(0,0,0,0.25)] flex items-center gap-4 w-fit"
           >
-            <FaEnvelope />
-            Skontaktuj się z nami
+            <FaEnvelope className="text-3xl" />
+            Skontaktuj się z&nbsp;nami
           </a>
         </div>
       </div>

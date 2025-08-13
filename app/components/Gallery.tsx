c3da917f6d68263e22f02c51f7d51932a658b4a6
ruffaaw <rafalciupek07@gmail.com>
2025-08-13 19:42:47 +0200

    feat: added animations

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { fadeIn } from "../animations/animations";
 
 const galleryImages = [
   { url: "/image2.png", title: "Obraz 2" },
@@ -54,13 +55,37 @@ export default function Gallery() {
       className="flex flex-col pb-24 bg-transparent bg-linear-to-b from-[#24425C] from-[38%] to-[var(--mainBg)] to-[74%] scroll-mt-24 md:scroll-mt-28"
     >
       <div className="flex flex-row items-center gap-5 w-full">
-        <div className="w-full md:w-1/5 h-[5px] md:h-[8px] bg-white" />
-        <h1 className="text-5xl md:text-8xl font-semibold text-white">
+        <motion.div
+          initial={{ scaleX: 0 }}
+          whileInView={{ scaleX: 1 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 0.6 }}
+          className="w-full md:w-1/5 h-[5px] md:h-[8px] bg-white"
+        />
+        <motion.h1
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.3 }}
+          className="text-5xl md:text-8xl font-semibold text-white"
+        >
           Galeria
-        </h1>
-        <div className="w-full md:w-4/5 h-[5px] md:h-[8px] bg-white" />
+        </motion.h1>
+        <motion.div
+          initial={{ scaleX: 0 }}
+          whileInView={{ scaleX: 1 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 0.6 }}
+          className="w-full md:w-4/5 h-[5px] md:h-[8px] bg-white"
+        />
       </div>
-      <div className="relative w-full max-w-7xl mx-auto flex flex-col items-center gap-4 pt-10">
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="relative w-full max-w-7xl mx-auto flex flex-col items-center gap-4 pt-10"
+      >
         <div className="relative w-full h-[30vh] sm:h-[50vh] md:h-[70vh] lg:h-[70vh] xl:h-[80vh] flex items-center justify-center overflow-visible">
           <button
             onClick={prevImage}
@@ -75,7 +100,7 @@ export default function Gallery() {
                 i === 0
                   ? "-translate-x-1/6 scale-90 z-0"
                   : i === 2
-                  ? "translate-x-1/6 scale-90  z-0"
+                  ? "translate-x-1/6 scale-90 z-0"
                   : "scale-100 z-10";
 
               return (
@@ -102,7 +127,7 @@ export default function Gallery() {
             <FaChevronRight size={32} />
           </button>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
